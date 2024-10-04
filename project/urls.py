a15fd74ac7223394fc506485f4b1a2f4cab3f19d
plt <p.tareev@puls.ru>
2024-10-04 19:13:33 +0300

    added swagger

--- project/urls.py
+++ project/urls.py
@@ -16,7 +16,23 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework.permissions import AllowAny
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Company API",
+      default_version='v1',
+      contact=openapi.Contact(email="test-task@gmail.com"),
+      license=openapi.License(name="MIT License"),
+   ),
+   public=True,
+   permission_classes=[AllowAny]
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin-dashboard/', admin.site.urls),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
